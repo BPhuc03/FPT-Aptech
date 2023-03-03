f270b1811067a3d4110eb7c6a42a70133d60d1d7
BPhuc03 <buihuong2551976@gmail.com>
2023-03-03 09:19:52 +0700

    g

--- Baithi/TestBank.java
+++
@@ -1,8 +0,0 @@
-package Baithi;
-
-public class TestBank{
-    public static void main(String[] args) {
-        Bank  b = new Bank(1000, 100);
-        System.out.println("Monthly interest is : " + b.calculateInterest());
-    }
-}
