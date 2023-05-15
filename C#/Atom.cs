31568e89cc871668a54ab93a8e2050904c296789
BPhuc03 <buihuong2551976@gmail.com>
2023-05-15 11:51:44 +0700

    C#

---
+++ C#/Atom.cs
@@ -0,0 +1,50 @@
+﻿using System.Collections;
+
+class Atomic
+{
+    private int atomicNumber;
+    private string symbol;
+    private string fullName;
+    private double weight;
+
+    public Atomic(int atomicNumber, string symbol, string fullName, double weight)
+    {
+        this.atomicNumber = atomicNumber;
+        this.symbol = symbol;
+        this.fullName = fullName;
+        this.weight = weight;
+    }
+
+    public Atomic()
+    {
+    }
+    ArrayList arr = new ArrayList();
+    public bool accept()
+    {
+        System.Console.WriteLine("Enter atomic number : ");
+
+        atomicNumber = Convert.ToInt32(System.Console.ReadLine());
+        System.Console.WriteLine("Enter symbol : ");
+        symbol = Console.ReadLine();
+
+        System.Console.WriteLine("Enter full name : ");
+        fullName = Console.ReadLine();
+
+        System.Console.WriteLine("Enter atomic weight :  ");
+        weight = Convert.ToDouble(System.Console.ReadLine());
+
+
+        Atomic atm = new Atomic(atomicNumber, symbol, fullName, weight);
+        arr.Add(atm);
+        return true;
+
+    }
+
+    public void display()
+    {
+        foreach (Atomic at in arr)
+        {
+            System.Console.WriteLine(at.atomicNumber + " " + at.symbol + " " + at.fullName + " " + at.weight);
+        }
+    }
+}
---
+++ C#/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab3c
+{
+        class Program
+        {
+            public static void Main(string[] args)
+            {
+                Atomic atm = new Atomic();
+                int i = 0;
+
+                do
+                {
+                    i++;
+                    atm.accept();
+
+                } while (i != 3);
+                System.Console.WriteLine(" ");
+                atm.display();
+
+
+            }
+        }
+ }
